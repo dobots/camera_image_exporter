8419e014d10eabbfdd15b97c5f899cee0c93cc06
root <ldania>
2022-02-17 13:28:15 +0000

    Changes to be committed: 	modified:   ../.gitignore 	new file:   save 	modified:   save_video.sh

--- video/save_video.sh
+++ video/save_video.sh
@@ -5,41 +5,60 @@ declare -i i=0
 
 while getopts "o:t:" flag
 do
-	case $flag in
-		o) 
-			parameter_output=${OPTARG};;
-			#echo "Name of output file $parameter_output"
-		t) 
-			
-			parameter_time=${OPTARG}
-			echo "Recording time $parameter_time seconds"
+         case $flag in
+                 o)
+                         parameter_output=${OPTARG}
+			 if [[ ! $parameter_output == .mp4 ]] && [ -z "parameter_output" ]
+  			 then
+          			parameter_output="$parameter_output.mp4"
+
+  			fi
+			if [ -f ./saved_videos/$parameter_output.mp4 ]
+ 			then
+ 				echo "The filename $parameter_output is already in use, choose a different name"
+ 				exit 0
+ 			fi
+
 			;;
-	esac
+                       #echo "Name of output file $parameter_output"
+                 t)
+ 
+                         parameter_time=${OPTARG}
+                         echo "Recording time $parameter_time seconds"
+                         ;;
+         esac
 done
 
-if [ -z "$parameter_time" ]
-then	
-	echo "No value given, Default Recording time 10"
-	parameter_time=10
+#if [[ ! $parameter_output == .mp4 ]] && [ -z "parameter_output" ]
+#then
+#	parameter_output="$parameter_output.mp4"
+#
+#fi
+
+
+
+if [ -f ./saved_videos/$parameter_output ]
+then
+	echo "The filename $parameter_output is already in use, choose a different name"
+	exit 0
 fi
 
-
-
+echo "$parameter_output"
 if [ -z "$parameter_output" ]
-then	
-	echo "No Output file given, Default file name MyVideo.mp4"
-	parameter_output="MyVideo.mp4"
+then
+while [ -f ./saved_videos/MyVideo$i.mp4 ]
+do
+	((i++)) 
+done
+echo "No output file given, your video will be saved as MyVideo$i.mp4"
+fi
+
+if [ -z "$parameter_time" ]
+then
+        echo "No value given, Default Recording time 10"
+         parameter_time=10
 fi
 
 
-#while [[ ! -f ./saved/MyOutput_$i.mp4 && -z "$parameter_output" ]]
-#do
-#	((i++))	
-#done	
-
-
-
-#echo MyOutput_$i
-
-ffmpeg -i "http://0.0.0.0:8080/stream?topic=/camera/image_raw" -t $parameter_time -f flv ../video/$parameter_output
+ffmpeg -i "http://0.0.0.0:8080/stream?topic=/camera/image_raw" -t $parameter_time -f flv ../video/$parameter_ou    tput
 
